c5df19434edc4572d26fd49443892648ce17be65
Marlon Kenny <marlonkenny@gmail.com>
2014-10-08 00:55:09 +0000

    error validation working

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -16,12 +16,23 @@ $(function() {
   });
 
   function validateForm(data) {
+    $(this).find('#contact-submit').button('loading')
     console.log('about to send with:' + data);
   };
 
-  function handleErrors(xhr, thrownError) {
-    console.log(xhr);
-    console.log(thrownError);
+  function handleErrors(thrownError) {
+    console.log(thrownError.responseText);
+    console.log(typeof thrownError.responseText);
+    var errors=eval("("+thrownError.responseText+")");
+    console.log(typeof errors);
+    $.each(errors, function(key, value){
+      input = $('#' + key);
+      if (input) {
+        var form_group = input.closest('.form-group');
+        form_group.addClass('has-error');
+        $("<span>").addClass('help-block').text("Error: " + value).appendTo(form_group);
+      }
+    });
   };
 
   function formSuccess() {
